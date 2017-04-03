63889ea8f05190da8345b430fc21c892d24a6b3e
stevezhao <steve.zhao@gmail.com>
2017-04-03 19:43:54 -0400

    FirebaseLogin

---
+++ app/src/main/java/com/example/android/firebaselogin/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.android.firebaselogin;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailEditText = (EditText) findViewById(R.id.edit_text_email);
+        passwordEditText = (EditText) findViewById(R.id.edit_text_password);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void logIn(View view) {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, task.getResult().getUser().getEmail() + " logged in successful",
+                                    Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+
+                    }
+                });
+    }
+}
